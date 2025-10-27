5312756bbfd03fed27d37a893762d1971c959aaa
marcoscassone02 <ma.cassone@alumno.um.edu.ar>
2025-10-27 19:17:15 -0300

    1er Issue completado

---
+++ src/test/java/com/miempresa/mi_app/MiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miempresa.mi_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
